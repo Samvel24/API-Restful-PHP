7db9593fdb950e3d652d875924474e3dc73035bd
SamuelR <s_ramirezg_0624@outlook.com>
2022-12-28 20:31:12 -0600

    Colocación de métodos para obtener información de la base de datos

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -24,8 +24,8 @@ class Conexion
             foreach($value as $value2) {
                 echo "Elemento " . $i . ": " .  $value2 . "<br>";
                 $i++;
+            }
         }
-    }
 
         */
         foreach($listaDatos as $key => $value) {
@@ -57,11 +57,66 @@ class Conexion
         Con file_get_contents() se lee un archivo y devuelve como resultado un cadena
         que contiene los datos leídos, en este caso los datos leídos del archivo config
         */
-        $jsonData = file_get_contents($direccion . "/" ."config");
+        $jsonData = file_get_contents($direccion . "/" . "config");
         /* Con json_decode() convertimos un string codificado en JSON a una variable de PHP
         Además, se usa true para convertir los datos json en un array asociativo
         */
         return json_decode($jsonData, true);
+    }
+
+    private function convertirAUTF8($array) {
+        /*
+        Con array_walk_recursive() se aplica una función de usuario (de manera recursiva)
+        a cada miembro de un array, en este caso se realiza de manera recursiva la detección
+        de codificación de caracteres (verifica si es utf-8 de manera estricta 
+        (tercer argumento con valor de true)) en el arreglo $array
+        Nota: La variable $item se pasa por referencia (&) para que el callback function() 
+        pueda modificarla internamente
+        */
+        array_walk_recursive($array, function(&$item, $key) {
+            if(!mb_detect_encoding($item, 'utf-8', true)) {
+                /* en caso de que $item no sea utf-8, lo convertimos a esa codificación 
+                con utf8_encode()
+                */
+                $item = utf8_encode($item);
+            }
+        });
+
+        return $array;
+    }
+
+    public function obtenerDatos($consulta) {
+        $results = $this->conexion->query($consulta);
+        $resultArray = array();
+
+        foreach ($results as $key) {
+            // colocamos cada llave ($key) dentro de un elemento de $resultArray
+            $resultArray[] = $key;
+        }
+
+        return $this->convertirAUTF8($resultArray);
+    }
+
+    public function filasAfectadas($consulta) {
+        $results = $this->conexion->query($consulta);
+        /* Se devuelven el número de filas afectadas en la última operación MySQL
+        ya sea INSERT, UPDATE, REPLACE o DELETE.
+        */
+        return $this->conexion->affected_rows();
+    }
+
+    public function idUltimaConsulta($consulta) {
+        $results = $this->conexion->query($consulta);
+        $filas = $this->conexion->affected_rows();
+
+        /*
+        Con insert_id() devolvemos el ID generado por la última consulta (normalmente 
+        INSERT) en una tabla con una columna que tenga el atributo AUTO_INCREMENT.
+        Si no se enviaron declaraciones INSERT o UPDATE a través de esta conexión, o si la tabla 
+        modificada no tiene una columna con el atributo AUTO_INCREMENT, esta función 
+        devolverá cero.
+        */
+        return $this->conexion->insert_id();
     }
 }
 
